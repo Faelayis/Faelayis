24247a0e0783de2dc3d51d4147cbfac4f2f0c4ab
Fae <Faelayis.dev.github@outlook.co.th>
2026-06-21 14:41:16 +0700

    Self-host site fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
 	import "../app.css";
+	import "$styles/fonts.css";
+	import frauncesLatinUrl from "$lib/fonts/fraunces-latin.woff2?url";
 	let { children } = $props();
 </script>
+
+<svelte:head>
+	<link rel="preload" href={frauncesLatinUrl} as="font" type="font/woff2" crossorigin="anonymous" />
+</svelte:head>
 
 {@render children()}
